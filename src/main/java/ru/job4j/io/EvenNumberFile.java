a8483cad1418f384cceb3e2363eb4655c1b5be26
arganaft <e1mar.arganaft@gmail.com>
2023-05-12 17:54:48 +0300

    2.1. FileInputStream

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try(FileInputStream in = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] strArr = text.toString().split(System.lineSeparator());
+            text.setLength(0);
+            for (String s : strArr) {
+                read = Integer.parseInt(s);
+                text.append(read);
+                text.append(read % 2 == 0 ? " - Число является четным" : " - Число является нечетным");
+                System.out.println(text);
+                text.setLength(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
