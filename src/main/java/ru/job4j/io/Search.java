e5ce87bc939f594d5182edaf2aedaa869fa23688
arganaft <e1mar.arganaft@gmail.com>
2023-06-28 01:47:38 +0300

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (!Files.isDirectory(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Root argument is not Directory. Usage  ROOT_FOLDER.");
+        }
+        if (args.length ==1) {
+            throw new IllegalArgumentException("Не указанно расширение файла");
+        }
+        if (!args[1].startsWith(".") || args[1].length() > 4) {
+            throw new IllegalArgumentException("Расширение файла должно начинаться с \".\" и " +
+                    "содержать от 1го до 3х символов латинского алфавита ");
+        }
     }
 }
