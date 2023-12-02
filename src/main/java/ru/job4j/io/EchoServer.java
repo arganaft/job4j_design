a9416d6acfb0ba374bd9bdf84ee232a6ebfa7eb5
arganaft <e1mar.arganaft@gmail.com>
2023-12-03 01:46:02 +0300

    2.2.2. Socket 3. Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        boolean greetings = true;
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,10 +14,24 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("/?msg=Bye")) {
-                            server.close();
-                        }
+
+                    String str = in.readLine();
+                    if (str.contains("/?msg=Hello") && !greetings) {
+                        out.write("Hello".getBytes());
+                    }
+                    if (greetings) {
+                        out.write("Hello, dear friend.".getBytes());
+                        greetings = false;
+                    }
+                    if (str.contains("/?msg=Exit")) {
+                        server.close();
+                        out.write("Shutting down the server...".getBytes());
+                    }
+
+                    if (!str.contains("/?msg=Hello") && !str.contains("/?msg=Exit")) {
+                        out.write("What".getBytes());
+                    }
+                    for (; str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
                     }
                     out.flush();
