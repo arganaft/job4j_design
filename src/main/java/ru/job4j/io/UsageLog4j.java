5143d6c192a936ab058469b7f83e45843c85ce91
arganaft <e1mar.arganaft@gmail.com>
2023-12-05 00:29:40 +0300

    2.2.3. Логгирование 3. Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        byte course = 2;
+        char gender = 'm';
+        short grade = 5426;
+        long scholarship = 3254263L;
+        float wage = 43534435.34543F;
+        double revenue = 26254625634263.4263532;
+
+
+        LOG.debug("User info name : {}, age : {}, course : {}, gender : {}, grade : {}", name, age, course, gender, grade);
+        LOG.error("Profit info -> scholarship : {}, wage : {}, revenue : {}", scholarship, wage, revenue);
     }
 }
