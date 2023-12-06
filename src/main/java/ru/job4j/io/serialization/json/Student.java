4cf30cbec079d605812bcaf4af643c084840b571
arganaft <e1mar.arganaft@gmail.com>
2023-12-06 22:02:45 +0300

    2.2.4. Сериализация 4. JAXB. Преобразование XML в POJO.

--- src/main/java/ru/job4j/io/serialization/json/Student.java
+++ src/main/java/ru/job4j/io/serialization/json/Student.java
@@ -1,17 +1,34 @@
-package ru.job4j.serialization.json;
+package ru.job4j.io.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+@XmlRootElement(name = "student")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
-    private final int course;
-    private final boolean stateEmployee;
-    private final String name;
-    private final Contact contact;
-    private final String[] subjects;
+
+    @XmlAttribute
+    private int course;
+
+    @XmlAttribute
+    private boolean stateEmployee;
+
+    @XmlAttribute
+    private String name;
+    private Contact contact;
+    @XmlElementWrapper(name = "subjects")
+    @XmlElement(name = "subject")
+    private String[] subjects;
+
+    public Student() { }
 
     public Student(int course, boolean stateEmployee, String name, Contact contact, String[] subjects) {
         this.course = course;
@@ -50,10 +67,24 @@ public class Student {
                 + " </subjects>"
                 + System.lineSeparator()
                 + "</student>";
-
     }
 
-    public static void main(String[] args) {
+    public String getXML() throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(Student.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String result = "";
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(this, writer);
+            result = writer.getBuffer().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws JAXBException {
         final Student student1 = new Student(3,
                 true,
                 "Steve",
@@ -66,5 +97,7 @@ public class Student {
         Student studentFromGson = gson.fromJson(student1ToGson, Student.class);
         System.out.println(studentFromGson);
         System.out.println(student1.toXML());
+
+        System.out.println(student1.getXML());
     }
 }
