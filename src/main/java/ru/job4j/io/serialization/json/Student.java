6fd0b84fb5c354696c375baaae630d161a446828
arganaft <e1mar.arganaft@gmail.com>
2023-12-07 01:26:46 +0300

    2.2.4. Сериализация 5. Преобразование JSON в POJO. JsonObject

--- src/main/java/ru/job4j/io/serialization/json/Student.java
+++ src/main/java/ru/job4j/io/serialization/json/Student.java
@@ -2,13 +2,17 @@ package ru.job4j.io.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.*;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @XmlRootElement(name = "student")
@@ -28,9 +32,29 @@ public class Student {
     @XmlElement(name = "subject")
     private String[] subjects;
 
+    public int getCourse() {
+        return course;
+    }
+
+    public boolean isStateEmployee() {
+        return stateEmployee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getSubjects() {
+        return subjects;
+    }
+
     public Student() { }
 
-    public Student(int course, boolean stateEmployee, String name, Contact contact, String[] subjects) {
+    public Student(int course, boolean stateEmployee, String name, Contact contact, String... subjects) {
         this.course = course;
         this.stateEmployee = stateEmployee;
         this.name = name;
@@ -49,55 +73,36 @@ public class Student {
                 + '}';
     }
 
-    public String toXML() {
-        return "<?xml version=\"1.1\" encoding=\"UTF-8\" ?>"
-                + System.lineSeparator()
-                + "<student course=\"" + course + "\" "
-                + "stateEmployee=\"" + stateEmployee + "\" "
-                + "name=\"" + name + "\">"
-                + System.lineSeparator()
-                + " " + contact.toXMLNoVersion() + "/>"
-                + System.lineSeparator()
-                + " <subjects>"
-                + System.lineSeparator()
-                + Arrays.stream(subjects)
-                .map(subject -> "  <subject>" + subject + "</subject>")
-                .collect(Collectors.joining(System.lineSeparator()))
-                + System.lineSeparator()
-                + " </subjects>"
-                + System.lineSeparator()
-                + "</student>";
-    }
-
-    public String getXML() throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(Student.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String result = "";
-
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(this, writer);
-            result = writer.getBuffer().toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
 
     public static void main(String[] args) throws JAXBException {
-        final Student student1 = new Student(3,
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"81234567890\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("mathematics");
+        list.add("history");
+        list.add("cybernetics");
+        JSONArray jsonSubjects = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Student student = new Student(3,
                 true,
                 "Steve",
                 new Contact("81234567890"),
-                new String[] {"mathematics", "history", "cybernetics"});
-        System.out.println(student1);
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(student1));
-        final String student1ToGson = gson.toJson(student1);
-        Student studentFromGson = gson.fromJson(student1ToGson, Student.class);
-        System.out.println(studentFromGson);
-        System.out.println(student1.toXML());
+                "mathematics", "history", "cybernetics");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("course", student.course);
+        jsonObject.put("stateEmployee", student.stateEmployee);
+        jsonObject.put("stateEmployee", student.stateEmployee);
+        jsonObject.put("name", student.name);
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonSubjects);
 
-        System.out.println(student1.getXML());
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект student в json-строку */
+        System.out.println(new JSONObject(student).toString());
     }
 }
