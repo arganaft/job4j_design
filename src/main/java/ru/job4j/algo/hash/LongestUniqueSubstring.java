95e86d004ad7070384d3a2fbff749ab1df8b1d2a
arganaft <e1mar.arganaft@gmail.com>
2025-03-21 13:09:47 +0300

    2.6.2. Hash структуры Алгоритмы с Hash структурами

---
+++ src/main/java/ru/job4j/algo/hash/LongestUniqueSubstring.java
@@ -0,0 +1,43 @@
+package ru.job4j.algo.hash;
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LongestUniqueSubstring {
+    public static String longestUniqueSubstring(String str) {
+        Set<Character> result = new LinkedHashSet<>();
+        Set<Character> temp = new LinkedHashSet<>();
+        for (char ch : str.toCharArray()) {
+            if (!temp.add(ch)) {
+                if (temp.size() > result.size()) {
+                    rewriteResult(result, temp);
+                }
+                temp.add(ch);
+            }
+        }
+        if (result.size() == 0 || temp.size() > result.size()) {
+            rewriteResult(result, temp);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : result) {
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
+    private static void rewriteResult(Set<Character> result, Set<Character> temp) {
+        result.clear();
+        result.addAll(temp);
+        temp.clear();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "cbaebabacdcbar";
+        String anagramIndices = longestUniqueSubstring(s);
+        System.out.println(anagramIndices);
+    }
+
+}
